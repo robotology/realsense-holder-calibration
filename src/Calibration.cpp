62b6c9c4eeed47450a1f4685b38d9ca09a8ca14a
Nicola Piga <nicolapiga@gmail.com>
2021-07-08 07:02:47 +0200

    Cleanup

--- src/Calibration.cpp
+++ src/Calibration.cpp
@@ -5,129 +5,66 @@
  * GPL-2+ license. See the accompanying LICENSE file for details.
  */
 
-#include "Calibration.h"
+#include <Calibration.h>
+
+#include <fstream>
+
+#include <yarp/os/Bottle.h>
+
+using namespace yarp::sig;
+using namespace yarp::os;
+using namespace iCub::iKin;
 
 
-bool Calibration::configure(yarp::os::ResourceFinder &rf)
+bool Calibration::configure(ResourceFinder &rf)
 {
     /* Get the robot name. */
-    robot_name_ = rf.check("icubSim", yarp::os::Value("icubSim")).asString();
+    robot_name_ = rf.check("robot_name", Value("icub")).asString();
 
-    /* Check if the robot is the driver are open for the torso. */
-    if(!Calibration::OpenRemoteControlboard(robot_name_, "torso"))
+    /* Get the wait time. */
+    wait_time_ = rf.check("wait_time", Value(5.0)).asDouble();
+
+    /* Get the number of poses. */
+    Value number_of_poses_value = rf.find("number_of_poses");
+    if (number_of_poses_value.isNull())
     {
-        return EXIT_FAILURE;
+        std::cerr << log_name_ + "::ctor(). Error: the provided configuration file does not specify the expected number of poses" << std::endl;
+        return false;
     }
+    else
+        number_of_poses_ = number_of_poses_value.asInt();
 
-    /* Check if the robot is the driver are open for the head. */
-    if(!Calibration::OpenRemoteControlboard(robot_name_, "head"))
-    {
-        return EXIT_FAILURE;
-    }
+    /* Retrieve joints configurations for calibration. */
+    if(!get_joints_configuration(rf))
+        return false;
 
-    /* Release the links. */
+    /* Open torso control board. */
+    if(!open_remote_control_board(robot_name_, "torso"))
+        return false;
+
+    /* Opem head control board. */
+    if(!open_remote_control_board(robot_name_, "head"))
+        return false;
+
+    /* Configure the forward kinematics. */
+    icub_head_center_ = iCubHeadCenter("right_v2");
     icub_head_center_.releaseLink(0);
     icub_head_center_.releaseLink(1);
     icub_head_center_.releaseLink(2);
 
-    /* Store the number of different poses necessary for the calibration. */
-    number_of_poses_ = rf.find("number_of_poses").asInt();
-
-    /* Point the group that contains the absolute angles of the joints. */
-    yarp::os::Bottle rf_poses_group = rf.findGroup("CALIBRATION_POSES");
-
-    /* Store the data from the config.ini file*/
-    Calibration::RetrieveTheData(rf_poses_group);
-
-    /* Set the initial state of the state machine. */
-    this->status_ = StateRobot::next_pose;
+    /* Set the initial state of the module. */
+    state_ = State::Idle;
 
     return true;
 }
 
-bool Calibration::RetrieveTheData(yarp::os::Bottle rf_poses_group)
+
+bool Calibration::close()
 {
-    for(int i = 0; i<number_of_poses_; i++)
-    {
-
-        if (rf_poses_group.find("Config"+std::to_string(i)).isNull())
-        {
-            throw(std::runtime_error("The key does not exist"));
-            return false;
-        }
-
-        /* Get the vector related to the ith pose. */
-        yarp::os::Bottle * b = rf_poses_group.find("Config"+std::to_string(i)).asList();
-
-        /* Temporary vector to store the data. */
-        std::vector<double> temp;
-
-        /* Check if the vector is nulla. */
-        if(b == nullptr)
-            return false;
-
-        for (std::size_t j=0; j<b->size(); j++)
-        {
-            /* Get the jth value of the single pose. */
-            yarp::os::Value item = b->get(j);
-
-            /*Check if the value is null. */
-            if (item.isNull())
-            {
-                throw(std::runtime_error("The value of the " +  std::to_string(j) + " element of the " +  std::to_string(i) + "configure is null"));
-                return false;
-            }
-
-            /* Push the value into the temporary vector. */
-            temp.push_back( item.asDouble());
-        }
-
-        /*Push the temporary vector into the class variable vect_. */
-        vect_.push_back(temp);
-
-    }
+    drivers_.at("torso").close();
+    drivers_.at("head").close();
 
     return true;
-
-}
-
-bool Calibration::OpenRemoteControlboard(const std::string & robot, const std::string & part_name)
-{
-    yarp::os::Property prop;
-
-    /* Set the property of the device. */
-    prop.put("device", "remote_controlboard");
-    prop.put("local", "/realsense-holder-calibration/" + part_name);
-    prop.put("remote", "/" + robot_name_ + "/" + part_name);
-
-    /* Check if the driver is open. */
-    if (!drivers_[part_name].open(prop))
-    {
-        std::cout << "Cannot open " + part_name + " driver." << std::endl;
-
-        return false;
-    }
-
-    /* Open the views. */
-    bool ok= true;
-    ok = ok && this->drivers_[part_name].view(this->encoders_[part_name]);
-    ok = ok && this->drivers_[part_name].view(this->control_mode_[part_name]);
-    ok = ok && this->drivers_[part_name].view(this->position_control_[part_name]);
-
-    /* Check if the views are open. */
-    if (!ok)
-    {
-            throw(std::runtime_error("Unable to open views"));
-    }
-
-    /* Set the control mode for the joints. */
-    this->control_mode_[part_name]->setControlMode(0, VOCAB_CM_POSITION);
-    this->control_mode_[part_name]->setControlMode(1, VOCAB_CM_POSITION);
-    this->control_mode_[part_name]->setControlMode(2, VOCAB_CM_POSITION);
-
-    return true;
-
-
 }
 
 
@@ -136,107 +73,186 @@ double Calibration::getPeriod()
     return 1.0;
 }
 
+
 bool Calibration::updateModule()
 {
-
-
-    switch(this->status_)
+    switch(state_)
     {
-        case StateRobot::idle:
+        case State::Idle:
+            /* Do nothing */
+            break;
+
+        case State::NextPose:
         {
+            /* Store start time, required for the timer. */
+            start_time_ = std::chrono::steady_clock::now();
 
-            std::cout <<" Calibration done! " << std::endl;
-            return true;
+            /* Set the new position of the joints. */
+            position_control_["torso"]->positionMove(0, joints_[counter_poses_][0]);
+            position_control_["torso"]->positionMove(1, joints_[counter_poses_][1]);
+            position_control_["torso"]->positionMove(2, joints_[counter_poses_][2]);
+            position_control_["head"]->positionMove(0, joints_[counter_poses_][3]);
+            position_control_["head"]->positionMove(1, joints_[counter_poses_][4]);
+            position_control_["head"]->positionMove(2, joints_[counter_poses_][5]);
 
+            state_ = State::Wait;
+
+            /* Increment the counter of the pose. */
+            counter_poses_++;
+
+            break;
         }
-        case StateRobot::next_pose:
+
+        case State::Wait:
         {
-            /* Store the time value from when we wait for 5 seconds. */
-            start1_ = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now());
+            /* Check if wait time elapsed. */
+            if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start_time_).count() > wait_time_)
+                state_ = State::Store;
 
-            /*Set the new position of the joints. */
-            this->position_control_["torso"]->positionMove(0, vect_[counter_poses_][0]);
-            this->position_control_["torso"]->positionMove(1, vect_[counter_poses_][1]);
-            this->position_control_["torso"]->positionMove(2, vect_[counter_poses_][2]);
-            this->position_control_["head"]->positionMove(0, vect_[counter_poses_][3]);
-            this->position_control_["head"]->positionMove(1, vect_[counter_poses_][4]);
-            this->position_control_["head"]->positionMove(2, vect_[counter_poses_][5]);
-
-            /* Set the next state of the state machine. */
-            this->status_ = StateRobot::wait_pose;
-
+            break;
         }
-        case StateRobot::wait_pose:
+
+        case State::Store:
         {
-            /* Check if 5 seconds have passed. */
-            if ( std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()  - start1_).count() >= 5 )
-            {
-                /* Initialize the vector to store the values of the joints. */
-                yarp::sig::Vector chain;
+            /* Get the end-effector pose. */
+            Matrix H = ee_pose();
 
-                /* Max number of joints to set. */
-                int max_size = 3;
+            /* Open the file where to store the pose. */
+            std::ofstream pose_file;
 
-                /*Get the position of the joints. */
-                for (const std::string part_name : {"torso", "head"})
-                {
-                    int size;
-                    encoders_[part_name]->getAxes(&size);
+            /* Store the pose, counting from zero. */
+            pose_file.open("pose_fPe_" + std::to_string(counter_poses_ - 1) + ".yaml");
+            pose_file << H.toString() << std::endl;
 
-                    yarp::sig::Vector part_chain(size);
-                    encoders_[part_name]->getEncoders(part_chain.data());
+            /* Close the file. */
+            pose_file.close();
 
-                    int offset = 0;
-                    if (part_name == "torso")
-                        offset = -2;
+            /* TODO: get the input image and save it on disk. */
 
-                    for (std::size_t i = 0; i < max_size; i++)
-                        chain.push_back(part_chain(abs(offset + i)));
-                }
-
-
-                std::cout<<"Loading done."<<std::endl;
-
-                /* Convert the values into a matrix wrt the reference system. */
-                yarp::sig::Matrix H = this->icub_head_center_.getH(chain * M_PI / 180.0);
-
-                /* Increment the counter of the pose. */
-                counter_poses_ ++;
-
-                /* Open the file where to store the pose. */
-                std::ofstream pose_file;
-                pose_file.open("pose_fPe_" + std::to_string(counter_poses_) + ".yaml");
-                std::cout <<" Creating pose file number " + std::to_string(counter_poses_) +"..."<< std::endl;
-                pose_file << H.toString() << std::endl;
-
-                /* Close the file. */
-                pose_file.close();
-
-                /* Check if the are any other configuration poses and set the next state. */
-                if (counter_poses_ < number_of_poses_)
-                {
-                    this->status_ = StateRobot::next_pose;
-                }
-                else
-                {
-                    this->status_ = StateRobot::idle;
-                }
-            }
-
-            /* Wait for the 5 seconds to end. */
+            /* Check if the are still configuration to be executed. */
+            if (counter_poses_ == number_of_poses_)
+                state_ = State::Idle;
             else
-            {
-                std::cout<<"Loading..."<<std::endl;
-            }
+                state_ = State::NextPose;
         }
 
+        break;
     }
-
-
 
     return true;
 }
-bool Calibration::close()
+
+
+bool Calibration::open_remote_control_board(const std::string& robot, const std::string& part_name)
 {
+    Property prop;
+
+    /* Set the property of the device. */
+    prop.put("device", "remote_controlboard");
+    prop.put("local", "/realsense-holder-calibration/" + part_name);
+    prop.put("remote", "/" + robot_name_ + "/" + part_name);
+
+    /* Try to open the driver. */
+    if (!drivers_[part_name].open(prop))
+    {
+        std::cerr << "Calibration::open_remote_control_board(). Error: cannot open " + part_name + " driver." << std::endl;
+        return false;
+    }
+
+    /* Open the views. */
+    bool ok = true;
+    ok &= drivers_[part_name].view(encoders_[part_name]);
+    ok &= drivers_[part_name].view(control_mode_[part_name]);
+    ok &= drivers_[part_name].view(position_control_[part_name]);
+    if (!ok)
+    {
+        std::cerr << "Calibration::open_remote_control_board(). Error: cannot open " + part_name + " interfaces." << std::endl;
+        return false;
+    }
+
+    /* Set the control mode for the joints. */
+    control_mode_[part_name]->setControlMode(0, VOCAB_CM_POSITION);
+    control_mode_[part_name]->setControlMode(1, VOCAB_CM_POSITION);
+    control_mode_[part_name]->setControlMode(2, VOCAB_CM_POSITION);
+
     return true;
+}
+
+
+bool Calibration::get_joints_configuration(const yarp::os::ResourceFinder& rf)
+{
+    const Bottle& poses_group = rf.findGroup("CALIBRATION_POSES");
+
+    for(std::size_t i = 0; i < number_of_poses_; i++)
+    {
+        Value configuration = poses_group.find("config_" + std::to_string(i));
+
+        if (configuration.isNull())
+        {
+            std::cerr << log_name_ + "::get_joints_configuration(). Error: joint configuration " + std::to_string(i) + " is missing." << std::endl;
+            return false;
+        }
+
+        /* Get the vector related to the i-th configuration. */
+        Bottle* b = configuration.asList();
+        if(b == nullptr)
+        {
+            std::cerr << log_name_ + "::get_joints_configuration(). Error: while reading joint configuration " + std::to_string(i) + "." << std::endl;
+            return false;
+        }
+
+        std::vector<double> entry;
+        for (std::size_t j = 0; j < b->size(); j++)
+        {
+            /* Get the jth value of the single pose. */
+            Value item = b->get(j);
+
+            /*Check if the value is null. */
+            if (item.isNull())
+            {
+                std::cerr << log_name_ + "::get_joints_configuration(). Error: cannot read "
+                          << std::to_string(j) + "-th element of the "
+                          << std::to_string(i) + "-th configuration." << std::endl;
+                return false;
+            }
+
+            entry.push_back(item.asDouble());
+        }
+
+        joints_.push_back(entry);
+    }
+
+    return true;
+}
+
+
+yarp::sig::Matrix Calibration::ee_pose()
+{
+    /* Initialize the vector to store the values of the joints. */
+    yarp::sig::Vector chain;
+
+    /* Max number of joints to set. */
+    int max_size = 3;
+
+    /* Get the position of the joints. */
+    for (const std::string part_name : {"torso", "head"})
+    {
+        int size;
+        encoders_[part_name]->getAxes(&size);
+
+        yarp::sig::Vector part_chain(size);
+        encoders_[part_name]->getEncoders(part_chain.data());
+
+        int offset = 0;
+        if (part_name == "torso")
+            offset = -2;
+
+        for (std::size_t i = 0; i < max_size; i++)
+            chain.push_back(part_chain(abs(offset + i)));
+    }
+
+    /* Evaluate the forward kinematics. */
+    yarp::sig::Matrix H = icub_head_center_.getH(chain * M_PI / 180.0);
+
+    return H;
 }
--- src/Calibration.h
+++ src/Calibration.h
@@ -4,24 +4,24 @@
  * This software may be modified and distributed under the terms of the
  * GPL-2+ license. See the accompanying LICENSE file for details.
  */
- #ifndef CALIBRATION_H
- #define CALIBRATION_H
 
-#include <bits/stdc++.h>
+#ifndef CALIBRATION_H
+#define CALIBRATION_H
+
 #include <chrono>
-#include <cstdlib>
 #include <iostream>
-#include <fstream>
-#include <cmath>
-#include <thread>
 #include <unordered_map>
 
 #include <iCub/iKin/iKinFwd.h>
-#include <stdexcept>
 #include <vector>
-#include <yarp/os/all.h>
-#include <yarp/dev/all.h>
-#include <yarp/sig/all.h>
+
+#include <yarp/os/ResourceFinder.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/dev/IEncoders.h>
+#include <yarp/dev/IControlMode.h>
+#include <yarp/dev/IPositionControl.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/sig/Matrix.h>
 
 
 class Calibration : public yarp::os::RFModule
@@ -31,48 +31,54 @@ private:
     /* Store the name of the robot. */
     std::string robot_name_;
 
-    /* Varibale which describe the kinematic of the straight line coming out from the pooint located between the eyes. */
+    /* Forward kinematics of the robot neck. */
     iCub::iKin::iCubHeadCenter icub_head_center_;
 
-    /* Initialize devices to control the robot position. */
+    /* Devices required to control the robot position. */
     std::unordered_map<std::string, yarp::dev::PolyDriver> drivers_;
     std::unordered_map<std::string, yarp::dev::IEncoders*> encoders_;
     std::unordered_map<std::string, yarp::dev::IControlMode*> control_mode_;
     std::unordered_map<std::string, yarp::dev::IPositionControl*> position_control_;
 
-    /* Initialize the time variable. */
-    std::chrono::system_clock::time_point start1_;
+    /* Timer related. */
+    std::chrono::steady_clock::time_point start_time_;
 
-    /* Storage for the number of configuration poses. */
+    double wait_time_;
+
+    /* Number of configuration poses. */
     int number_of_poses_;
 
-    /* Counter of the poses. */
+    /* Counter for number of poses. */
     int counter_poses_ = 0;
 
-    /* Storage for the poses. */
-    std::vector<std::vector<double>> vect_;
+    /* Storage for the joints configurations. */
+    std::vector<std::vector<double>> joints_;
 
-    /* Enumeration class and related variable for the state machine states. */
-    enum class StateRobot{idle, next_pose, wait_pose};
-    StateRobot status_;
+    /* Module state. */
+    enum class State{Idle, NextPose, Wait, Store};
+    State state_;
 
-    /* Open the Devices. */
-    bool OpenRemoteControlboard(const std::string &, const std::string&);
+    /* Name to be used in messages. */
+    const std::string log_name_ = "Calibration";
 
-    /* Retrieve the data from the config.ini file. */
-    bool RetrieveTheData(yarp::os::Bottle  rf_poses_group);
+    /* Open robot devices. */
+    bool open_remote_control_board(const std::string&, const std::string&);
+
+    /* Retrieve joints configurations for calibration. */
+    bool get_joints_configuration(const yarp::os::ResourceFinder& rf);
+
+    /* Retrieve the end effector pose. */
+    yarp::sig::Matrix ee_pose();
 
 public:
-    
-    bool configure(yarp::os::ResourceFinder &rf);
 
-    bool updateModule();
+    bool configure(yarp::os::ResourceFinder& rf) override;
 
-    double getPeriod();
+    bool close() override;
 
-    bool close();
+    double getPeriod() override;
 
-
-
+    bool updateModule() override;
 };
-#endif /* CALIBRATION_H. */
+
+#endif /* CALIBRATION_H */
