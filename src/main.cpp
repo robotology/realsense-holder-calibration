523b3ac8c4c96711d5f3c833a13f5d5d93093841
Nicola Piga <nicolapiga@gmail.com>
2021-07-08 07:10:08 +0200

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,33 @@
-#include <chrono>
+/*
+ * Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
 #include <cstdlib>
 #include <iostream>
-#include <cmath>
-#include "Calibration.h"
-#include <thread>
-#include <unordered_map>
 
-#include <iCub/iKin/iKinFwd.h>
+#include <Calibration.h>
 
-#include <yarp/dev/IEncoders.h>
-#include <yarp/dev/PolyDriver.h>
-#include <yarp/os/Property.h>
 #include <yarp/os/ResourceFinder.h>
-#include <yarp/sig/Matrix.h>
-#include <yarp/sig/Vector.h>
+
+using namespace yarp::os;
+
 
 int main (int argc, char** argv)
 {
-    yarp::os::Network yarp;
+    Network yarp;
     if (!yarp.checkNetwork())
     {
-        std::cout<<"YARP doesn't seem to be available"<<std::endl;
+        std::cerr << "YARP network not available." << std::endl;
+
         return EXIT_FAILURE;
     }
 
-    Calibration mod;
-    yarp::os::ResourceFinder rf;
-
-    //rf.setDefaultContext("test");
-    //rf.setDefaultConfigFile("config.ini");
+    ResourceFinder rf;
     rf.configure(argc, argv);
-    return mod.runModule(rf);
 
+    Calibration module;
+    return module.runModule(rf);
 }
