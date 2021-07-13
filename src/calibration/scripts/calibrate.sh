a7ea8a2b958a9a08a2433442cec86e2ccd385552
Nicola Piga <nicolapiga@gmail.com>
2021-07-13 09:58:49 +0200

    Update calibration script

--- src/calibration/scripts/calibrate.sh
+++ src/calibration/scripts/calibrate.sh
@@ -1,11 +1,27 @@
-VISP_BUILD_DIR=?
-CAMERA_POSE_ESTIMATOR_PATH=$VISP_BUILD_DIR/tutorial-chessboard-pose
-CALIBRATOR_PATH=$VISP_BUILD_DIR/tutorial-hand-eye-calibration
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
 
-W=?
-H=?
-SIZE=?
-N_POSES=?
+if [ $# -ne 6 ]
+then
+    echo "Synopsis: bash calibrate.sh <path_to_visp_build> <path_to_images> <number_poses> <width> <height> <square_size (meters)>"
+    exit 1
+fi
 
-$CAMERA_POSE_ESTIMATOR_PATH -w $W -h $H --square_size $SIZE --camera_name Camera --intrinsic ./camera.xml --input image-%d.png
-$CALIBRATOR_PATH --ndata $N_POSES
+VISP_BUILD_DIR=$1
+IMAGES_PATH=$2
+N_POSES=$3
+W=$4
+H=$5
+SIZE=$6
+
+CAMERA_POSE_ESTIMATOR_PATH=$VISP_BUILD_DIR/tutorial/calibration/tutorial-chessboard-pose
+CALIBRATOR_PATH=$VISP_BUILD_DIR/tutorial/calibration/tutorial-hand-eye-calibration
+
+(cd $IMAGES_PATH && $CAMERA_POSE_ESTIMATOR_PATH -w $W -h $H --square_size $SIZE --camera_name Camera --intrinsic ./camera.xml --input ./image-%d.png)
+(cd $IMAGES_PATH && $CALIBRATOR_PATH --ndata $N_POSES)
